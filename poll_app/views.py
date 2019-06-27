651a65296ee7eefdec7078004c6db657a3f2a723
EvyHaan <evy.haan@gmail.com>
2019-06-27 12:39:06 -0700

    updated views

--- poll_app/views.py
+++ poll_app/views.py
@@ -1,21 +1,36 @@
-# from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
+from .models import Question
 
-from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world! -Dex")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_question_list': latest_question_list,
+        }
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question {question_id}.")
+    question = get_object_or_404(Question, pk=question_id)
+    context = {
+        'question': question,
+        }
+    return render(request, 'polls/detail.html', context)
 
 
 def results(request, question_id):
-    response = "You're looking at the results of question {question_id}."
-    return HttpResponse(response)
+    question = Question.objects.get(pk=question_id)
+    context = {
+        'question': question,
+        }
+    return render(request, 'polls/results.html', context)
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question {question_id}.")
+    question = Question.objects.get(pk=question_id)
+    context = {
+        'question': question,
+        }
+    return render(request, 'polls/vote.html', context)
 
